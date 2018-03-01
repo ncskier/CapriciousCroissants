131a20a9e5d62465a41655579076c5d411526490
Brandon Walker <bcw58@cornell.edu>
2018-02-28 21:47:52 -0500

    Get rid of matches before game begins

--- source/TileBoard.cpp
+++ source/TileBoard.cpp
@@ -20,6 +20,7 @@ _numPawns(4) {
 	colorLookup = {Color4::WHITE, Color4::RED, Color4::BLACK, Color4::MAGENTA, Color4::BLUE};
     srand((int)time(NULL));
     generateNewBoard();
+    while (checkForMatches());
 	srand((int)time(NULL));
 }
 

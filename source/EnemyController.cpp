9d8f2eed65b56b967b01fb120f35f2ff39986f47
Brandon Walker <bcw58@cornell.edu>
2018-03-11 14:54:17 -0400

    Created templates for PlayerController, EnemyController, and BoardController

--- source/EnemyController.cpp
+++ source/EnemyController.cpp
@@ -7,3 +7,61 @@
 //
 
 #include "EnemyController.h"
+using namespace cugl;
+
+#pragma mark -
+#pragma mark Constructors
+/**
+ * Creates a new game world with the default values.
+ *
+ * This constructor does not allocate any objects or start the controller.
+ * This allows us to use a controller without a heap pointer.
+ */
+EnemyController::EnemyController() {
+    
+}
+
+/**
+ * Initializes the controller contents, and starts the enemy turn
+ *
+ * The constructor does not allocate any objects or memory.  This allows
+ * us to have a non-pointer reference to this controller, reducing our
+ * memory allocation.  Instead, allocation happens in this method.
+ *
+ * @param board    The game board
+ *
+ * @return true if the controller is initialized properly, false otherwise.
+ */
+bool EnemyController::init(const std::shared_ptr<BoardModel>& board) {
+    _board = board;
+    
+    return true;
+}
+
+/**
+ * Disposes of all (non-static) resources allocated to this mode.
+ */
+void EnemyController::dispose() {
+    _board = nullptr;
+}
+
+
+#pragma mark -
+#pragma mark Gameplay Handling
+/**
+ * The method called to update the enemy turn.
+ *
+ * This method contains any gameplay code that is not an OpenGL call.
+ *
+ * @param timestep  The amount of time (in seconds) since the last frame
+ */
+void EnemyController::update(float timestep) {
+    
+}
+
+/**
+ * Resets the status of the game so that we can play again.
+ */
+void EnemyController::reset() {
+    
+}
--- source/PlayerController.cpp
+++ source/PlayerController.cpp
@@ -7,3 +7,65 @@
 //
 
 #include "PlayerController.h"
+
+using namespace cugl;
+
+#pragma mark -
+#pragma mark Constructors
+/**
+ * Creates a new game world with the default values.
+ *
+ * This constructor does not allocate any objects or start the controller.
+ * This allows us to use a controller without a heap pointer.
+ */
+PlayerController::PlayerController() {
+    
+}
+
+/**
+ * Initializes the controller contents, and starts the player turn
+ *
+ * The constructor does not allocate any objects or memory.  This allows
+ * us to have a non-pointer reference to this controller, reducing our
+ * memory allocation.  Instead, allocation happens in this method.
+ *
+ * @param board    The game board
+ * @param input    The input controller
+ *
+ * @return true if the controller is initialized properly, false otherwise.
+ */
+bool PlayerController::init(const std::shared_ptr<BoardModel>& board, const std::shared_ptr<InputController>& input) {
+    _board = board;
+    _input = input;
+    
+    return true;
+}
+
+/**
+ * Disposes of all (non-static) resources allocated to this mode.
+ */
+void PlayerController::dispose() {
+    _board = nullptr;
+    _input = nullptr;
+}
+
+
+#pragma mark -
+#pragma mark Gameplay Handling
+/**
+ * The method called to update the player turn.
+ *
+ * This method contains any gameplay code that is not an OpenGL call.
+ *
+ * @param timestep  The amount of time (in seconds) since the last frame
+ */
+void PlayerController::update(float timestep) {
+    
+}
+
+/**
+ * Resets the status of the game so that we can play again.
+ */
+void PlayerController::reset() {
+    
+}

a28337113d7358df1e62fbaeb5f305ebf0ed0f0b
Tyler Thompson <tt395@cornell.edu>
2018-03-12 01:17:07 -0400

    Fixed for phone

--- source/BoardSettingsMode.cpp
+++ source/BoardSettingsMode.cpp
@@ -16,21 +16,19 @@ using namespace cugl;
 #define SCENE_WIDTH 1024
 
 /**
- * Initializes the controller contents, making it ready for loading
- *
- * The constructor does not allocate any objects or memory.  This allows
- * us to have a non-pointer reference to this controller, reducing our
- * memory allocation.  Instead, allocation happens in this method.
- *
- * @param assets    The (loaded) assets for this game mode
- *
- * @return true if the controller is initialized properly, false otherwise.
- */
+* Initializes the controller contents, making it ready for loading
+*
+* The constructor does not allocate any objects or memory.  This allows
+* us to have a non-pointer reference to this controller, reducing our
+* memory allocation.  Instead, allocation happens in this method.
+*
+* @param assets    The (loaded) assets for this game mode
+*
+* @return true if the controller is initialized properly, false otherwise.
+*/
 bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) {
 	// Initialize the scene to a locked width
 	Size dimen = Application::get()->getDisplaySize();
-	int appHeight = Application::get()->getDisplayHeight();
-	int scale = SCENE_WIDTH / dimen.width;
 	dimen *= SCENE_WIDTH / dimen.width; // Lock the game to a reasonable resolution
 	if (assets == nullptr) {
 		return false;
@@ -49,30 +47,30 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	Vec2 position;
 
 	_heightLabel = std::dynamic_pointer_cast<Label>(assets->get<Node>("settings_labelheight"));
-	_heightLabel->setPosition(position.set(626, 576 - 97).scale(scale));
+	_heightLabel->setPosition(position.set(626, 576 - 97));
 	_widthLabel = std::dynamic_pointer_cast<Label>(assets->get<Node>("settings_labelwidth"));
-	_widthLabel->setPosition(position.set(626, 576 - 171).scale(scale));
+	_widthLabel->setPosition(position.set(626, 576 - 171));
 	_colorsLabel = std::dynamic_pointer_cast<Label>(assets->get<Node>("settings_labelcolors"));
-	_colorsLabel->setPosition(position.set(626, 576 - 288).scale(scale));
+	_colorsLabel->setPosition(position.set(626, 576 - 288));
 	_alliesLabel = std::dynamic_pointer_cast<Label>(assets->get<Node>("settings_labelallies"));
-	_alliesLabel->setPosition(position.set(626, 576 - 360).scale(scale));
+	_alliesLabel->setPosition(position.set(626, 576 - 360));
 
 	_checkmark = std::dynamic_pointer_cast<PolygonNode>(assets->get<Node>("settings_check"));
-	_checkmark->setPosition(position.set(594, 576 - 505).scale(scale));
+	_checkmark->setPosition(position.set(594, 576 - 505));
 	_checkmark->setVisible(placePawns);
 
-	
+
 	_heightLabel->setText(std::to_string(height), true);
 	_widthLabel->setText(std::to_string(width), true);
 	_colorsLabel->setText(std::to_string(colors), true);
 	_alliesLabel->setText(std::to_string(allies), true);
-	
 
-	
+
+
 
 	_upHeightButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_upheight"));
-	_upHeightButton->setPosition(position.set(734, 576 - 93).scale(scale));
-	_upHeightButton ->setListener([=](const std::string& name, bool down) {
+	_upHeightButton->setPosition(position.set(734, 576 - 93));
+	_upHeightButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			height++;
 			if (height > 10) {
@@ -84,7 +82,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_upHeightButton->activate(1);
 
 	_downHeightButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_downheight"));
-	_downHeightButton->setPosition(position.set(734, 576 - 125).scale(scale));
+	_downHeightButton->setPosition(position.set(734, 576 - 125));
 	_downHeightButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			height--;
@@ -97,7 +95,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_downHeightButton->activate(2);
 
 	_upWidthButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_upwidth"));
-	_upWidthButton->setPosition(position.set(734, 576 - 168).scale(scale));
+	_upWidthButton->setPosition(position.set(734, 576 - 168));
 	_upWidthButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			width++;
@@ -110,7 +108,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_upWidthButton->activate(3);
 
 	_downWidthButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_downwidth"));
-	_downWidthButton->setPosition(position.set(734, 576 - 200).scale(scale));
+	_downWidthButton->setPosition(position.set(734, 576 - 200));
 	_downWidthButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			width--;
@@ -123,7 +121,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_downWidthButton->activate(4);
 
 	_upColorsButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_upcolors"));
-	_upColorsButton->setPosition(position.set(734, 576 - 285).scale(scale));
+	_upColorsButton->setPosition(position.set(734, 576 - 285));
 	_upColorsButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			colors++;
@@ -136,7 +134,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_upColorsButton->activate(5);
 
 	_downColorsButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_downcolors"));
-	_downColorsButton->setPosition(position.set(734, 576 - 316).scale(scale));
+	_downColorsButton->setPosition(position.set(734, 576 - 316));
 	_downColorsButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			colors--;
@@ -149,7 +147,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_downColorsButton->activate(6);
 
 	_upAlliesButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_upallies"));
-	_upAlliesButton->setPosition(position.set(734, 576 - 358).scale(scale));
+	_upAlliesButton->setPosition(position.set(734, 576 - 358));
 	_upAlliesButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			allies++;
@@ -162,7 +160,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_upAlliesButton->activate(7);
 
 	_downAlliesButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_downallies"));
-	_downAlliesButton->setPosition(position.set(734, 576 - 390).scale(scale));
+	_downAlliesButton->setPosition(position.set(734, 576 - 390));
 	_downAlliesButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			allies--;
@@ -175,7 +173,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_downAlliesButton->activate(8);
 
 	_placePawnsButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_placepawns"));
-	_placePawnsButton->setPosition(position.set(590, 576 - 512).scale(scale));
+	_placePawnsButton->setPosition(position.set(590, 576 - 512));
 	_placePawnsButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			placePawns = !placePawns;
@@ -185,7 +183,7 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_placePawnsButton->activate(9);
 
 	_doneButton = std::dynamic_pointer_cast<Button>(assets->get<Node>("settings_done"));
-	_doneButton->setPosition(position.set(835, 576 - 561).scale(scale));
+	_doneButton->setPosition(position.set(835, 576 - 561));
 	_doneButton->setListener([=](const std::string& name, bool down) {
 		if (down) {
 			this->isDone = true;
@@ -194,18 +192,18 @@ bool BoardSettingsMode::init(const std::shared_ptr<cugl::AssetManager>& assets) 
 	_doneButton->activate(10);
 
 
-	
+
 	Application::get()->setClearColor(Color4(192, 192, 192, 255));
-	
+
 	return true;
 }
 
 /**
- * Disposes of all (non-static) resources allocated to this mode.
- */
+* Disposes of all (non-static) resources allocated to this mode.
+*/
 void BoardSettingsMode::dispose() {
-    // Deactivate the button (platform dependent)
-	
+	// Deactivate the button (platform dependent)
+
 	if (_upHeightButton) {
 		_upHeightButton->deactivate();
 	}
@@ -250,18 +248,18 @@ void BoardSettingsMode::dispose() {
 	_colorsLabel = nullptr;
 	_alliesLabel = nullptr;
 
-    _assets = nullptr;
+	_assets = nullptr;
 }
 
 
 #pragma mark -
 #pragma mark Progress Monitoring
 /**
- * The method called to update the game mode.
- *
- * This method updates the progress bar amount.
- *
- * @param timestep  The amount of time (in seconds) since the last frame
- */
+* The method called to update the game mode.
+*
+* This method updates the progress bar amount.
+*
+* @param timestep  The amount of time (in seconds) since the last frame
+*/
 void BoardSettingsMode::update(float progress) {
 }

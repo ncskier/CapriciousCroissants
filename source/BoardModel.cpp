2880c6b732f82c0060a929b5530cb690eed6fd8c
Brandon Walker <bcw58@cornell.edu>
2018-03-29 15:52:26 -0400

    Fix selecting the correct tile (can be tweaked in future if it's off). I shifted the touch down

--- source/BoardModel.cpp
+++ source/BoardModel.cpp
@@ -504,6 +504,7 @@ void BoardModel::slide(int offset) {
 bool BoardModel::selectTileAtPosition(Vec2 position) {
     int x;
     int y;
+    position = position + Vec2(0.0f, -getCellLength()*0.15f*2.0f);
     std::tie(x, y) = screenToGrid(position);
     if (x < 0 || _width <= x) {
         return false;
